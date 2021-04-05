a191f2fbd0b925902d60bd7b7b4a63d4858fb45f
Cesar Cuellar <raseckuat@gmail.com>
2021-04-05 14:11:49 -0400

    Agregando RangoVaricion.py

---
+++ RangoVariacion.py
@@ -0,0 +1,49 @@
+print("Escribir un programa en el cual: dada una lista de tres valores numericos distintos se calcule e informe su rango de variacion (debe mostrar el mayor y el menor de ellos)")
+print("***********************************************************************************************************************")
+print("***********************************************************************************************************************")
+print("***********************************************************************************************************************")
+first= input("Ingresa el primer valor")
+second= input("Ingresa el segundo valor")
+third= input("Ingresa el tercer valor")
+
+
+if first > second:
+    if first> third: 
+        mayor=first
+    else:
+        mayor=third
+
+else: 
+    if second >third:
+        mayor= second
+    else: 
+        mayor = third
+
+if first < second:
+    if first < third: 
+        minor= first
+    else:
+        minor=third
+        
+else: 
+    if second < third:
+        minor= second
+    else: 
+        minor = third
+
+
+#print(f"El rango de los numero es: [{minor},{mayor}], de los numeros ingresados: {first},{second},{third}")
+print(f"Esto esta raro {minor}")
+
+
+
+
+
+
+
+
+
+
+
+
+
